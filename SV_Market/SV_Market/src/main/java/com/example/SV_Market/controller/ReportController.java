515c9a2e2b65910469c497d6c4f3ae1b776f0e4b
MeoSuytGay <hung1961dn@gmail.com>
2024-10-07 14:32:32 +0700

    update Cross

--- SV_Market/SV_Market/src/main/java/com/example/SV_Market/controller/ReportController.java
+++ SV_Market/SV_Market/src/main/java/com/example/SV_Market/controller/ReportController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/reports")
 public class ReportController {
     @Autowired
