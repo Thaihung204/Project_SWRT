6015eebaccdd6ba6cd2123611e9d7568d5f04066
Le Tuan Kiet <kietltde180282@fpt.edu.vn>
2024-10-09 11:55:52 +0700

    Update view Report

--- SV_Market/SV_Market/src/main/java/com/example/SV_Market/service/ReportService.java
+++ SV_Market/SV_Market/src/main/java/com/example/SV_Market/service/ReportService.java
@@ -1,15 +1,20 @@
 package com.example.SV_Market.service;
 
 
+import com.example.SV_Market.entity.Product;
 import com.example.SV_Market.entity.Report;
+import com.example.SV_Market.repository.ProductRepository;
 import com.example.SV_Market.repository.ReportRepository;
 import com.example.SV_Market.request.FeedbackCreationRequest;
+import com.example.SV_Market.request.ReportResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.SV_Market.request.ReportCreationRequest;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ReportService {
@@ -19,6 +24,7 @@ public class ReportService {
     UserService userService;
     @Autowired
     ProductService productService;
+
 
     public Report createReport(@RequestBody ReportCreationRequest request) {
         Report report = new Report();
@@ -32,8 +38,22 @@ public class ReportService {
         return reportRepository.save(report);
     }
 
-    public List viewHistoryReport() {
-        return  reportRepository.findAll();
-    }
 
+    public List<ReportResponse> viewHistoryReport(String userId) {
+        List<Report> reports = reportRepository.findByUser_UserId(userId);
+
+        // Sử dụng Stream API để chuyển đổi danh sách Report sang ReportResponse
+        return reports.stream().map(report -> {
+            ReportResponse response = new ReportResponse();
+            response.setReportId(report.getReportId());
+            response.setTitle(report.getTitle());
+            response.setDescription(report.getDescription());
+            response.setType(report.getType());
+            response.setResponseMessage(report.getResponseMessage());
+            response.setUser(report.getUser());
+            response.setProductName(report.getProduct().getProductName());  // Lấy tên sản phẩm từ product
+            return response;
+        }).collect(Collectors.toList());
+    }
 }
+
