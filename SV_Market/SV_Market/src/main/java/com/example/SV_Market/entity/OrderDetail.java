dd22ca102b5fb37a543fa1d0091cf88b603ba275
Thaihung204 <hunghbtde180329@fpt.edu.vn>
2024-10-09 10:36:47 +0700

    update entity order/orderdetail

--- SV_Market/SV_Market/src/main/java/com/example/SV_Market/entity/OrderDetail.java
+++ SV_Market/SV_Market/src/main/java/com/example/SV_Market/entity/OrderDetail.java
@@ -28,12 +28,9 @@ public class OrderDetail {
     @JoinColumn(name = "product_id", nullable = false, referencedColumnName = "product_id")
     private Product product;
 
-    @Column(name = "type", length = 50, nullable = false)
-    private String type;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trade_product_id", nullable = false, referencedColumnName = "product_id")
+    private Product tradeProduct;
 
-    @Column(name = "start_date", nullable = false)
-    private LocalDate startDate;
 
-    @Column(name = "end_date")
-    private LocalDate endDate;
 }
