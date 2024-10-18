8e27506dda7051da9411577f11aac605b7cb5252
thaihung.204 <hunghbtde180329@fpt.edu.vn>
2024-10-18 16:41:31 +0700

    up image/ delete image

--- SV_Market/SV_Market/src/main/java/com/example/SV_Market/service/ProductImageService.java
+++ SV_Market/SV_Market/src/main/java/com/example/SV_Market/service/ProductImageService.java
@@ -3,6 +3,7 @@ package com.example.SV_Market.service;
 import com.example.SV_Market.entity.Product;
 import com.example.SV_Market.entity.ProductImage;
 import com.example.SV_Market.repository.ProductImageRepository;
+import com.example.SV_Market.request.ProductImageCreationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,40 +15,26 @@ import java.util.List;
 public class ProductImageService {
     @Autowired
     ProductImageRepository productImageRepository;
+    @Autowired
+    CloudinaryService cloudinaryService;
+    @Autowired
+    ProductService productService;
 
-    public List<ProductImage> createProductImage(Product product, List<String> images){
-        List<ProductImage> imagesReturn = new ArrayList<>();
-        for(String image : images) {
-            ProductImage productImage = new ProductImage();
-            productImage.setProduct(product);
-            productImage.setPath(image);
-            imagesReturn.add(productImage);
-            productImageRepository.save(productImage);
-        }
-        return imagesReturn;
+    public ProductImage createProductImage(ProductImageCreationRequest request){
+        String imagePath = cloudinaryService.upload(request.getImage());  // Iterate over each image path from the request
+            ProductImage productImage = new ProductImage();  // Create a new ProductImage object
+            productImage.setPath(imagePath);  // Set the image path
+            productImage.setProduct(productService.getProductById(request.getProductId()));  // Associate the image with the product
+
+        return productImageRepository.save(productImage);
     }
 
     public List<ProductImage> getImages(String productId) {
         return (List<ProductImage>) productImageRepository.findById(productId).orElseThrow(() -> new RuntimeException("ProductImage Not Found!"));
     }
 
+    public void deleteProductImage(String imageId){
+        productImageRepository.deleteById(imageId);
+    }
 
-
-//    public Product updateProduct(String productId, ProductUpdateRequest request){
-//        Product product = getProduct(productId);
-//
-//        LocalDate currentDate = LocalDate.now();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//        String formattedDate = currentDate.format(formatter);
-//
-//        product.setProductName(request.getProductName());
-//        product.setQuantity(request.getQuantity());
-//        product.setPrice(request.getPrice());
-//        product.setDescription(request.getDescription());
-//        product.setCategoryId(request.getCategoryId());
-//        product.setState(request.getState());
-//        product.setCreate_at(currentDate);
-//
-//        return productRepository.save(product);
-//    }
 }
